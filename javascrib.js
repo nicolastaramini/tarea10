664b49723db761b96e9a6b982fe9b5dea7ec1673
nicolastaramini <130618802+nicolastaramini@users.noreply.github.com>
2023-05-18 00:24:03 -0300

    Add files via upload

---
+++ javascrib.js
@@ -0,0 +1,12 @@
+var nombre="nicolas"
+var apellido="taramini"
+
+console.log(nombre+"  "+apellido)
+
+
+var numero1=33
+var numero2=20
+var suma= numero1 + numero2;
+console.log(suma)
+var sumada= suma+350
+console.log(sumada)
